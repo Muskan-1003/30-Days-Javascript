ac5652e816927bd10f2c91bd63dd59210059a1fd
Muskan Pandey <muskanpandey1003@gmail.com>
2023-11-06 23:05:44 +0530

    Create Day7.js

---
+++ Day7.js
@@ -0,0 +1,17 @@
+//Array Reduce Transformation
+
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let res=init;
+
+    for(const n of nums){
+        res=fn(res,n)
+    }
+    return res
+    
+};
