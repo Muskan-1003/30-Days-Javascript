661991f9e298c83546eabb46a1cf1128af45ee85
Muskan Pandey <muskanpandey1003@gmail.com>
2023-11-10 23:13:34 +0530

    Create Day11.js

---
+++ Day11.js
@@ -0,0 +1,31 @@
+ // Memoize
+
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    let map = {};
+    return function(...args) {
+        const s = args.toString();
+        if(s in map){
+            return map[s];
+        }else{
+            let res = fn(...args);
+            map[s] = res;
+            return res;
+        }
+            }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
