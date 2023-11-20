bf32aca5567ec0c675e3fc7fe3f816f1d480145d
Muskan Pandey <muskanpandey1003@gmail.com>
2023-11-20 23:58:20 +0530

    Create Day13.js

---
+++ Day13.js
@@ -0,0 +1,12 @@
+ // Sleep
+/**
+ * @param {number} millis
+ */
+async function sleep(millis) {
+    await new Promise(resolve => setTimeout(resolve, millis));
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
