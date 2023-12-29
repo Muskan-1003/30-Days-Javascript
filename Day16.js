ed800e0922eb6d8122b447817985e148467e7a25
Muskan Pandey <muskanpandey1003@gmail.com>
2023-12-29 23:40:26 +0530

    Create Day16.js

---
+++ Day16.js
@@ -0,0 +1,20 @@
+//2637. Promise Time Limit
+
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+	return async function(...args) {
+        const originalFnPromise = fn(...args);
+
+        const timeoutPromise = new Promise((_, reject) => {
+            setTimeout(() => {
+                reject('Time Limit Exceeded')
+            }, t);
+        })
+
+        return Promise.race([originalFnPromise, timeoutPromise]);
+    }
+};
